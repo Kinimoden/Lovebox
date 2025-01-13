924f52cdf6960634000eeee2ec8bea0a71d57a04
Kinimoden <dom.11szla@gmail.com>
2025-01-13 19:37:56 +0100

    credentials.h

--- credentials.h
+++ credentials.h
@@ -1,5 +1,5 @@
-const char* _ssid = "WLAN_NAME";
-const char* _password = "PASSWORD";
+const char* _ssid = "kamera_na_somsiadki";
+const char* _password = "badoszla99";
 
 // url to gist: /yourUsername/generatedHashValue/raw/yourFilename
 const String _url = "PATH_tO_YOUR_GIST";
